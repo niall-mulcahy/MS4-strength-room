3688ef3121033dc05cf8fdca9fb7ca4d58bce164
niall-mulcahy <niall-mulcahy@outlook.com>
2021-08-06 14:37:46 +0000

    added product checkout styling and stripe card input element

--- products/views.py
+++ products/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
+from django.conf import settings
 from .models import Product
 from .forms import NewOrderForm
+import stripe
 
 
 def products(request):
@@ -21,9 +23,13 @@ def product_checkout(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
     order_form = NewOrderForm()
 
+    stripe_total = product.price
+
     context = {
         'product': product,
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51J9omdJq4HTAYf7lueZYAvtrZobGmqSRbBHLdQHtOLAKNn86VA2lNidxBAKf092yMIL4jVvUPdigVYUg18OxRSae00TDu9dySF',
+        'client_secret': 'Test client',
     }
 
     return render(request, 'products/product_checkout.html', context)
