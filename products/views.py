b9c21380402ecf2f9c692cfc1000893d23a58df1
niall-mulcahy <niall-mulcahy@outlook.com>
2021-08-08 12:44:34 +0000

    added some new info to product_checkout page

--- products/views.py
+++ products/views.py
@@ -44,18 +44,27 @@ def product_checkout(request, product_id):
         county = request.POST.get("county")
         country = request.POST.get("country")
         postcode = request.POST.get("postcode")
-        Order.objects.get_or_create(
-            product=product,
-            full_name=full_name,
-            email=email,
-            street_address1=street_address1,
-            street_address2=street_address2,
-            town_or_city=town_or_city,
-            county=county,
-            country=country,
-            postcode=postcode,
-            order_total=product.price
-        )
+        if order_form.is_valid:
+            order = Order.objects.get_or_create(
+                product=product,
+                full_name=full_name,
+                email=email,
+                phone_number=phone_number,
+                street_address1=street_address1,
+                street_address2=street_address2,
+                town_or_city=town_or_city,
+                county=county,
+                country=country,
+                postcode=postcode,
+                order_total=product.price
+            )
+            request.session['save_info'] = 'save-info' in request.POST
+            print(order[1])
+            return redirect(
+                reverse('checkout_success', args=[order[0]]))
+        else:
+            messages.error(request, 'There was an error with your form. \
+                Please double check your information.')
 
     context = {
         'product': product,
@@ -74,6 +83,7 @@ def checkout_success(request, order_number):
 
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
+    product = get_object_or_404(Product, order.product)
     messages.success(request, f'Order successfully processed! \
         Your order number is {order_number}. A confirmation \
         email will be sent to {order.email}.')
@@ -82,6 +92,7 @@ def checkout_success(request, order_number):
 
     context = {
         'order': order,
+        'product': product,
     }
 
     return render(request, template, context)
