1f87e53d67426871648d605ed2eb0aac6f1fd240
niall-mulcahy <niall-mulcahy@outlook.com>
2021-08-21 10:11:52 +0000

    Removed secret key and set debug

--- strength_room/settings.py
+++ strength_room/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
-import json
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-su=2amkm8(hsmmi0m7)l9cbob3cs$v4^g9en*s*km*6nl-2_w2'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['ms4-strength-room.herokuapp.com', 'localhost']
 
